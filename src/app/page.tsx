2af30bf3e73c2a992676f91c1e35559b21e4b530
fahad-islam <fahadislam.dev@gmail.com>
2023-06-27 10:57:51 +0500

    add runtime 'edge' for / page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 
+export const runtime = "edge"; // 'nodejs' (default) | 'edge'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
